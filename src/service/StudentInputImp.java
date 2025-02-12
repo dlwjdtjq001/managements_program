7ae2a6bb1f4c242172560d0cfef35f77c940ba6e
sminseong <star919m@gmail.com>
2025-02-12 19:44:46 +0900

    0212_edit2

--- src/service/StudentInputImp.java
+++ src/service/StudentInputImp.java
@@ -8,16 +8,50 @@ import java.util.Scanner;
 
 public class StudentInputImp implements StudentInput {
     StudentIO studentIO;
+    private static int studentNumberCounter = 250001;
 
+    //studentNumber,name,korean,english,math,science,total,average,grade
     public StudentInputImp(StudentIO studentIO) {
         this.studentIO = studentIO;
     }
 
-    //studentNumber,name,korean,english,math,science,total,average,grade
-    //output에서 입력받은 객체 map에 삽입 (StudentNumber 생성)
-    public void inputStudent(String StudentNumber, String name, int korean, int english, int math, int science, int total, int average, String grade) {
-        StudentDto studentDto = new StudentDto(StudentNumber, name, korean, english, math, science, total, average, grade);
-        studentIO.getStudentTable().put(StudentNumber,studentDto);
+    public int calcTotal(StudentDto studentDto) {
+        return studentDto.getKorean() + studentDto.getEnglish() + studentDto.getMath() + studentDto.getScience();
     }
-    //중복값을 입력받았을 때, 오류 만들어서 output으로 throw
+    public double calcAverage(StudentDto studentDto) {
+        return studentDto.getTotal() / 4.0;
+    }
+    public String generateStudentNumber() {
+        return String.valueOf(studentNumberCounter++);
+    }
+
+    public void inputStudent(StudentDto studentDto) {
+        String newStudentNumber = generateStudentNumber();
+        StudentDto completeStudent = createPerfectDto(newStudentNumber, studentDto);
+        studentIO.updateStudentTable(completeStudent);
+    }
+
+    public StudentDto createPerfectDto(String studentNumber, StudentDto studentDto){
+        return StudentDto.builder()
+                .studentNumber(studentNumber)
+                .name(studentDto.getName())
+                .korean(studentDto.getKorean())
+                .english(studentDto.getEnglish())
+                .math(studentDto.getMath())
+                .science(studentDto.getScience())
+                .total(calcTotal(studentDto))
+                .average(calcAverage(studentDto))
+                .grade(calcGrade(calcAverage(studentDto)))
+                .build();
+    }
+    public String calcGrade(double average) {
+        switch ((int) average / 10) {
+            case 10:
+            case 9: return "A";
+            case 8: return "B";
+            case 7: return "C";
+            case 6: return "D";
+            default: return "F";
+        }
+    }
 }
