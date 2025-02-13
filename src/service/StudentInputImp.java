c30a5e45f9f14c926ed29f52c3b8649231ef2a7b
sminseong <star919m@gmail.com>
2025-02-13 18:56:32 +0900

    0213_edit3

--- src/service/StudentInputImp.java
+++ src/service/StudentInputImp.java
@@ -5,6 +5,7 @@ import dto.StudentDto;
 import repository.StudentManager;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -12,18 +13,20 @@ public class StudentInputImp implements StudentInput {
     //studentNumber,name,korean,english,math,science,total,average,grade
 
     StudentIO studentIO;
-    private static int studentNumberCounter = 250001;
+    private static int studentNumberCounter = 0;
 
     public StudentInputImp(StudentIO studentIO) {
         this.studentIO = studentIO;
     }
 
-    public void inputStudent(StudentDto studentDto) {
-        if (isDuplicateStudent(studentDto)) {
-            throw new RuntimeException();
+    //학번(studentNumberCounter 생성)
+    public String initStudentNumberCounter() {
+        Map<String, StudentDto> studentDtoMap = studentIO.getStudentTable();
+        if(studentNumberCounter == 0) {
+            List<String> list = studentDtoMap.keySet().stream().toList();
+            studentNumberCounter = list.stream().mapToInt(x -> Integer.parseInt(x)).max().orElse(20250000);
         }
-        StudentDto completeStudent = createPerfectDto(studentDto);
-        updateStudentTable(completeStudent);
+        return String.valueOf(++studentNumberCounter);
     }
 
     //중복정보 검사 (학생이름 + 국어 + 영어 + 수학 + 과학)
@@ -46,6 +49,11 @@ public class StudentInputImp implements StudentInput {
         return studentDto.getKorean() + studentDto.getEnglish() + studentDto.getMath() + studentDto.getScience();
     }
 
+    //Average 계산
+    public double calcAverage(StudentDto studentDto) {
+        return calcTotal(studentDto) / 4.0;
+    }
+
     //Grade 계산
     public String calcGrade(double average) {
         switch ((int) (average / 10)) {
@@ -56,34 +64,5 @@ public class StudentInputImp implements StudentInput {
             case 6: return "D";
             default: return "F";
         }
-    }
-
-    //입력 받을때마다 studentNumberCounter(학번)에 1 더하고 String 으로 변환
-    public String generateStudentNumber() {
-        return String.valueOf(studentNumberCounter++);
-    }
-
-    // map(학번,DTO) put
-    private void updateStudentTable(StudentDto studentDto) {
-        StudentManager.getInstance().getStudentTable().put(studentDto.getStudentNumber(), studentDto);
-    }
-
-    //Builder
-    public StudentDto createPerfectDto(StudentDto studentDto){
-        int total = calcTotal(studentDto);
-        double average = total / 4.0;
-        String grade = calcGrade(average);
-
-        return StudentDto.builder()
-                .studentNumber(generateStudentNumber())
-                .name(studentDto.getName())
-                .korean(studentDto.getKorean())
-                .english(studentDto.getEnglish())
-                .math(studentDto.getMath())
-                .science(studentDto.getScience())
-                .total(total)
-                .average(average)
-                .grade(grade)
-                .build();
     }
 }
